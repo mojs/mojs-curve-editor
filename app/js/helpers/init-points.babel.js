eb882c5667bf05af49e5a94ea96a2abccdd9484e
Raminsiach <ramin.siach@gmail.com>
2017-03-15 13:52:24 +0100

    Making for loop more performant

--- app/js/helpers/init-points.babel.js
+++ app/js/helpers/init-points.babel.js
@@ -3,7 +3,7 @@ import initPoint from './init-point';
 export default (points) => {
 
   const newPoints = [];
-  for (var i = 0; i < points.length; i++) {
+  for (var i = 0, max = points.length; i < max; i++) {
     const point = points[i],
           sibPoint = (i === points.length-1)
             ? points[i-1] : points[i+1],
